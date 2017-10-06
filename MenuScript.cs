de6ca73d16651e92a3fee33dfe326b358f724a0b
ComputerGraphicsSessonalCSE414 <32571826+ComputerGraphicsSessonalCSE414@users.noreply.github.com>
2017-10-06 19:45:17 +0600

    Add files via upload

---
+++ MenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void ChangeScene(string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+}
